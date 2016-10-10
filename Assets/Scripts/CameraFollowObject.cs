7e27fc9655b974a51f7bc03a130509d9a1aa94fc
Matias <TeamPQZ@gmail.com>
2016-10-10 20:10:29 +0200

    meta n shit
    amemra

---
+++ Assets/Scripts/CameraFollowObject.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollowObject : MonoBehaviour
+{
+
+    public GameObject Target;
+    public float Speed;
+    public float XModifier;
+    public float YModifier;
+    public float ZModifier;
+    private Vector3 XYZMod;
+    
+
+    //Dette stykke kode fungerer til formålet,
+    //et 3D objekt, der kun skal bevæge sig fremad.
+    //Kameraet opfører sig modsat,
+    //hvis det peger bagud.
+
+    // Use this for initialization
+    void Start()
+    {
+        XYZMod = new Vector3(XModifier, YModifier, ZModifier);
+        transform.position = new Vector3(Target.transform.position.x, Target.transform.position.y, Target.transform.position.z) + XYZMod;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(Target.transform.position);
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            transform.RotateAround(Target.transform.position, new Vector3(Input.GetAxis("Mouse Y") * -1, Input.GetAxis("Mouse X"), 0), Speed * Time.deltaTime);
+        }
+        //Nedenstående lort redder dagen, i samarbejde med LateUpdate;
+    }
+}
---
+++ Assets/Scripts/RocketMovement.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RocketMovement : MonoBehaviour {
+
+    public float Speed;
+    public float RotationSpeed;
+
+    public int VertMod;
+    public int HoriMod;
+
+    public Rigidbody rb;
+
+    //Scriptet fungerer fint, så længe spilleren 
+    //kun bevæger sig ad en enkelt axis.
+    //Hvis spilleren roterer 180 grader, 
+    //begynder det at opføre sig lidt underligt.
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rb.velocity = transform.forward * Speed * Time.deltaTime;
+        transform.Rotate(new Vector3(Input.GetAxis("Vertical") * VertMod, Input.GetAxis("Horizontal") * HoriMod, 0) * RotationSpeed * Time.deltaTime, Space.Self);
+        Debug.Log(transform.rotation.eulerAngles.x);
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+
+    }
+
+    
+}
